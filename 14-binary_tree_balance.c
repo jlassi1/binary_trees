e7ecbd71da7b11943e1fd645e3f36773f0aae12c
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-22 17:12:35 +0100

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,24 +8,18 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-int nr = 0, nf = 0;
+int nr, nf;
 if (!tree)
 return (0);
-const binary_tree_t *tmp;
-tmp = tree;
-if (!tree->right && !tree->left)
-return (0);
-while (tmp)
-{
-nf++;
-tmp = tmp->left;
-}
-tmp = tree;
-while (tmp)
-{
-nr++;
-tmp = tmp->right;
-}
+if (tree->left)
+nf = binary_tree_height(tree->left) + 1;
+else
+nf = binary_tree_height(tree->left);
+
+if (tree->right)
+nr = binary_tree_height(tree->right) + 1;
+else
+nr = binary_tree_height(tree->right);
 
 return (nf - nr);
 }
