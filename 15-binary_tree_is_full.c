2b58bde8b32ca74a55699f6191aa73e16bde6b78
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-22 17:19:19 +0100

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,30 +10,30 @@ int binary_tree_is_full(const binary_tree_t *tree)
 int nr = 0, nf = 0;
 const binary_tree_t *tmp;
 tmp = tree;
-if(!tree->right && !tree->left)
-return(0);
+if (!tree->right && !tree->left)
+return (0);
 while (tmp->left)
 {
 nf++;
-if(!tmp->right && tmp->left)
-return(0);
-if(!tmp->left && tmp->right)
-return(0);
+if (!tmp->right && tmp->left)
+return (0);
+if (!tmp->left && tmp->right)
+return (0);
 tmp = tmp->left;
 }
 tmp = tree;
 while (tmp->right)
 {
 nr++;
-if(!tmp->right && tmp->left)
-return(0);
-if(!tmp->left && tmp->right)
-return(0);
+if (!tmp->right && tmp->left)
+return (0);
+if (!tmp->left && tmp->right)
+return (0);
 tmp = tmp->right;
 }
-if(nr == nf)
-return(1);
+if (nr == nf)
+return (1);
 
-return(0);
+return (0);
 
 }
