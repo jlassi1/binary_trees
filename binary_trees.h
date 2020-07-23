ed283627dc02b9f42b7c231a6c2c90b6930dec61
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-23 15:06:25 +0100

    header

--- binary_trees.h
+++ binary_trees.h
@@ -53,7 +53,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 
 bst_t *bst_insert(bst_t **tree, int value);
-
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
 #endif
