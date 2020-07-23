97a174b87cfd121b4d9784b0036a9df3a90f5924
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-23 14:35:57 +0100

    100-binary_trees_ancestor.c

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,52 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_uncle-finds the uncle of a node
+ *@node: a pointer to the node to find the uncle
+ *
+ *Return: a pointer to the uncle node or NULL if node has no uncle or NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+binary_tree_t *tmp;
+
+if (!node || !node->parent || !node->parent->parent)
+return (NULL);
+
+
+tmp = node->parent->parent;
+
+if (tmp->right == node->parent)
+return (node->parent->parent->left);
+
+if (tmp->left == node->parent)
+return (node->parent->parent->right);
+
+
+return (NULL);
+}
+/**
+ *binary_trees_ancestor-finds the lowest common ancestor of two nodes
+ *@first: a pointer to the first node
+ *@second: A pointer to the second node
+ *
+ *Return: a pointer to the lowest common ancestor node of the two given nodes
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
+{
+	if (second == first)
+		return ((binary_tree_t *)first);
+	if (second->parent == first)
+		return ((binary_tree_t *)first);
+	if (first->parent == second)
+		return ((binary_tree_t *)second);
+	if (first->parent == second->parent)
+		return ((binary_tree_t *)first->parent);
+	if ((binary_tree_t *)first == binary_tree_uncle((binary_tree_t *)second))
+		return ((binary_tree_t *)first);
+	if ((binary_tree_t *)second == binary_tree_uncle((binary_tree_t *)first))
+		return ((binary_tree_t *)second);
+
+	return (NULL);
+
+}
