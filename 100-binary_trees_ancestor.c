4a9814020e1a0a79f782a48cd0bc3fbc9422a089
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-24 12:29:12 +0100

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,30 +1,5 @@
 #include "binary_trees.h"
 /**
- *binary_tree_uncle-finds the uncle of a node
- *@node: a pointer to the node to find the uncle
- *
- *Return: a pointer to the uncle node or NULL if node has no uncle or NULL
- */
-binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-{
-binary_tree_t *tmp;
-
-if (!node || !node->parent || !node->parent->parent)
-return (NULL);
-
-
-tmp = node->parent->parent;
-
-if (tmp->right == node->parent)
-return (node->parent->parent->left);
-
-if (tmp->left == node->parent)
-return (node->parent->parent->right);
-
-
-return (NULL);
-}
-/**
  *binary_trees_ancestor-finds the lowest common ancestor of two nodes
  *@first: a pointer to the first node
  *@second: A pointer to the second node
